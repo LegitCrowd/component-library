74c0653b7d2b87513db8628b84a76d18198bfc6e
hasan-creatoros <hkhadra@creatoros.co>
2025-01-24 11:37:30 +0400

    fix rollup build issue

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,33 +1,31 @@
-'use strict';
-
-var jsxRuntime = require('react/jsx-runtime');
-var react = require('react');
+import { jsxs, jsx } from 'react/jsx-runtime';
+import { useState } from 'react';
 
 const ComponentA = ({
   initialState,
   onFinalState
 }) => {
-  const [state, setState] = react.useState(initialState);
+  const [state, setState] = useState(initialState);
   const getFinalState = () => {
     return state;
   };
   const handleLogFinalState = () => {
     onFinalState(state);
   };
-  return jsxRuntime.jsxs("div", {
-    children: [jsxRuntime.jsxs("p", {
+  return jsxs("div", {
+    children: [jsxs("p", {
       children: ["Current State: ", state]
-    }), jsxRuntime.jsx("button", {
+    }), jsx("button", {
       onClick: () => setState(state + 1),
       children: "Increment"
-    }), jsxRuntime.jsx("button", {
+    }), jsx("button", {
       onClick: () => console.log(getFinalState()),
       children: "Log Final State"
-    }), jsxRuntime.jsx("button", {
+    }), jsx("button", {
       onClick: handleLogFinalState,
       children: "Submit"
     })]
   });
 };
 
-exports.ComponentA = ComponentA;
+export { ComponentA };
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,40 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs',
-    exports: 'named'
-  },
-  plugins: [resolve(), typescript()],
-  external: ['react', 'react-dom']
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      exports: 'named'
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      exports: 'named'
+    }
+  ],
+  plugins: [
+    resolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist'
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      presets: [
+        '@babel/preset-react', 
+        '@babel/preset-typescript'
+      ],
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }),
+    commonjs()
+  ],
+  external: ['react', 'react-dom', 'react/jsx-runtime']
 };
